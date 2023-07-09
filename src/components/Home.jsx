7ae0ef4884dcb59d4a9b72863850318806681d9b
Nnaemeka Nnodim <mannynnodim@gmail.com>
2023-07-09 23:55:51 +0100

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,10 +7,10 @@ const Home = () => {
     <div name="home" className="w-full h-screen bg-[#0a192f]">
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full lg:px-16">
         <p className="text-pink-600">Hi, my name is </p>
-        <h1 className="text-4xl sm: text-7xl font-bold text-[#ccd6f6]">
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
           Nnodim Nnaemeka
         </h1>
-        <h2 className="text-4xl sm: text-7xl font-bold text-[#8892b0]">
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
           I am a Frontend Developer.
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
