4a9f0a647533fa90e9675beccfeb8d8744de9f00
Nnaemeka Nnodim <mannynnodim@gmail.com>
2023-07-10 11:44:56 +0100

    footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,37 +6,25 @@ import { HiOutlineMail } from "react-icons/hi"
 const Footer = () => {
   return (
     <div className=" lg:hidden w-full h-[80px] flex justify-center items-center px-4 bg-[#0a192f] text-gray-300">
-      <ul className="flex justify-evenly w-1/2 max-w-[1000px]">
+      <ul className="flex justify-evenly w-1/2 max-w-[900px]">
         <li>
-          <a
-            href="/"
-            className=" flex justify-between items-center w-full text-gray-300 "
-          >
-            <FaLinkedin size={30} />
+          <a href="/">
+            <FaLinkedin size={25} />
           </a>
         </li>
         <li>
-          <a
-            href="/"
-            className=" flex justify-between items-center w-full text-gray-300"
-          >
-            <FaGithub size={30} />
+          <a href="/">
+            <FaGithub size={25} />
           </a>
         </li>
         <li>
-          <a
-            href="/"
-            className=" flex justify-between items-center w-full text-gray-300"
-          >
-            <BsFillPersonLinesFill size={30} />
+          <a href="/">
+            <BsFillPersonLinesFill size={25} />
           </a>
         </li>
         <li>
-          <a
-            href="/"
-            className=" flex justify-between items-center w-full text-gray-300"
-          >
-            <HiOutlineMail size={30} />
+          <a href="/">
+            <HiOutlineMail size={25} />
           </a>
         </li>
       </ul>
